9c1f0220ae910ac0d5460974cb914aa0cc2fa489
Nicolas Guilbeault-Desjardins <dragoniko55@gmail.com>
2019-03-01 17:42:12 -0500

    Added folders in which we should store our scripts and the base class for activateable objects

---
+++ WonderJam2019/Assets/Scripts/Activateable/ActivateableComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class ActivateableComponent : MonoBehaviour
+{
+    /// <summary>
+    /// The sphere collider that determines the area in which the player controller displays a prompt to the player.
+    /// </summary>
+    public SphereCollider ActivationZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ActivationZone = this.GetComponent<SphereCollider>();
+    }
+}
