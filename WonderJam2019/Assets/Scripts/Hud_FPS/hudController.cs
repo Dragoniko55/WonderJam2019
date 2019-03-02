1cc015b500e32409f2973634be5cc6e4bcff1f4c
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-03-01 20:04:28 -0500

    ajout fps et hud
    Création d'un player avec un fpsController et un HUD

---
+++ WonderJam2019/Assets/Scripts/Hud_FPS/hudController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hudController : MonoBehaviour
+{
+    public Slider sliderOxygen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderOxygen.value = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void setOxygenValue(float oxygenValue = 1.0f)
+    {
+        if (oxygenValue < 0.0f) { oxygenValue = 0.0f; }
+        else if (oxygenValue > 1.0f) { oxygenValue = 1.0f; }
+        sliderOxygen.value = oxygenValue;
+    }
+}
