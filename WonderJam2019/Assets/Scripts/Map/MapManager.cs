aebf346e258224f9f34cc78a601e5022340c3de8
BigDaddyNIck <badkarma597@gmail.com>
2019-03-02 16:22:46 -0500

    Map Zoom

--- WonderJam2019/Assets/Scripts/Map/MapManager.cs
+++ WonderJam2019/Assets/Scripts/Map/MapManager.cs
@@ -18,7 +18,7 @@ public class MapManager : MonoBehaviour
     /// <summary>
     /// We have a maximum of 7 networks
     /// </summary>
-    public readonly Color[] NetworkColors = new Color[] 
+    public readonly Color[] NetworkColors = new Color[]
     {
         Color.blue,
         Color.cyan,
@@ -31,14 +31,19 @@ public class MapManager : MonoBehaviour
     };
 
     private bool showDescription = false;
+    private bool mapShown = true;
     private RectTransform descriptionPane;
     private MapObject selectedMapObject;
+    private RectTransform mapSurface;
 
     private void Start()
     {
         this.descriptionPane = FindObjectsOfType<RectTransform>().FirstOrDefault(o => o.name == DescriptionPaneName);
         if (this.descriptionPane is null) throw new System.NullReferenceException("Could not find the description pane.");
         this.descriptionPane.gameObject.SetActive(false);
+
+        this.mapSurface = FindObjectsOfType<RectTransform>().FirstOrDefault(rt => rt.name == "Map Content");
+        if (this.mapSurface is null) throw new System.NullReferenceException("Could not find the Map Content.");
     }
 
     public void ShowDescription(string description)
@@ -57,7 +62,7 @@ public class MapManager : MonoBehaviour
 
     public void Select(MapObject mapObject)
     {
-        if(this.selectedMapObject != null && mapObject != this.selectedMapObject)
+        if (this.selectedMapObject != null && mapObject != this.selectedMapObject)
         {
             this.selectedMapObject.OnUnselected();
         }
@@ -70,6 +75,32 @@ public class MapManager : MonoBehaviour
         if (this.showDescription)
         {
             this.descriptionPane.transform.position = (Vector2)Input.mousePosition - (this.descriptionPane.sizeDelta);
-        }        
+        }
+    }
+
+    private void Update()
+    {
+        if (this.mapShown)
+        {
+            float factor = 0.25f;
+
+            if (Input.GetKeyDown(KeyCode.KeypadPlus) || Input.GetKeyDown(KeyCode.Equals))
+            {
+                if (this.mapSurface.localScale.x < factor * 8)
+                {
+                    this.mapSurface.localScale += new Vector3(factor, factor, 0);
+                    this.mapSurface.position -= new Vector3(factor * this.mapSurface.sizeDelta.x / 2, factor / 2 * -1 * this.mapSurface.sizeDelta.y, 0);
+                }
+
+            }
+            else if (Input.GetKeyDown(KeyCode.KeypadMinus) || Input.GetKeyDown(KeyCode.Minus))
+            {
+                if (this.mapSurface.localScale.x > factor)
+                {
+                    this.mapSurface.localScale -= new Vector3(factor, factor, 0);
+                    this.mapSurface.position += new Vector3(factor * this.mapSurface.sizeDelta.x / 2, factor / 2 * -1 * this.mapSurface.sizeDelta.y, 0);
+                }
+            }
+        }
     }
 }
