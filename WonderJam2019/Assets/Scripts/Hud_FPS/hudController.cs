e3606274a47f484d393f9df5ebe66c666dc44713
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-03-01 20:50:43 -0500

    Ajout texte info
    Ajout d'un texte d'information dynamique, pouvant clignoter en rouge

--- WonderJam2019/Assets/Scripts/Hud_FPS/hudController.cs
+++ WonderJam2019/Assets/Scripts/Hud_FPS/hudController.cs
@@ -5,24 +5,55 @@ using UnityEngine.UI;
 
 public class hudController : MonoBehaviour
 {
-    public Slider sliderOxygen;
+    public Slider sliderOxygen; // Jauge representant le niveau oxygene
+    public Text textInfo; // Texte affichant les informations importantes et utiles
+
+    // TESTS & DEBUG
+
+    // FIN TESTS & DEBUG
 
     // Start is called before the first frame update
     void Start()
     {
-        sliderOxygen.value = 1.0f;
+        sliderOxygen.value = 1.0f; // Jauge peine au début
+        textInfo.text = "!!!!!!!!!"; // Texte vide au début
+
+        // TESTS & DEBUG
+        StartCoroutine("makeTextInfoBlink");
+        // FIN TESTS & DEBUG
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
+        // TESTS & DEBUG
+
+        // FIN TESTS & DEBUG
     }
 
-    void setOxygenValue(float oxygenValue = 1.0f)
+    public void setOxygenValue(float newOxygenValue = 1.0f)
     {
-        if (oxygenValue < 0.0f) { oxygenValue = 0.0f; }
-        else if (oxygenValue > 1.0f) { oxygenValue = 1.0f; }
-        sliderOxygen.value = oxygenValue;
+        if (newOxygenValue < 0.0f) { newOxygenValue = 0.0f; }
+        else if (newOxygenValue > 1.0f) { newOxygenValue = 1.0f; }
+        sliderOxygen.value = newOxygenValue;
+    }
+
+    public void setTextInfo(string newText = "", bool textBlink = true)
+    {
+        textInfo.text = newText;
+        if (textBlink) { StartCoroutine("makeTextInfoBlink"); }
+    }
+
+    IEnumerator makeTextInfoBlink()
+    {
+        while (true)
+        {
+            textInfo.color = new Color(255, 0, 0);
+            yield return new WaitForSeconds(.1f);
+            textInfo.color = new Color(0, 0, 0);
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
