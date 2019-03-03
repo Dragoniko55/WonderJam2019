bd4ba46c2beae62c7b0076820401542011e614fd
Samuel <airborne342@gmail.com>
2019-03-02 19:45:52 -0500

    collectible

---
+++ WonderJam2019/Assets/Scripts/ComsScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComsScript : MonoBehaviour
+{
+    bool isrepair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isrepair = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        if (isrepair)
+        {
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            Debug.Log("WARNING COMSCONSOLE BROKEN CHANGE CIRCUIT");
+        }
+    }
+
+    public void repair()
+    {
+        isrepair = true;
+    }
+}
---
+++ WonderJam2019/Assets/collectibleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectibleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] Linkedspawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomint = Random.Range(0, Linkedspawner.Length);
+        Debug.Log(randomint);
+        int cpt = 0;
+        foreach(GameObject spawn in Linkedspawner)
+        {
+
+            if (cpt != randomint)
+            {
+                spawn.GetComponent<collectibleScript>().desablecollectible();
+            }
+            cpt++;
+        }
+    }
+}
